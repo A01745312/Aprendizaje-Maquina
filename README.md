af76fc690e7e9d99172593af783e10865781bf80
Paula Santoyo <a01745312@itesm.mx>
2023-09-04 09:13:45 -0600

    Entrega 2

--- README.md
+++ README.md
@@ -7,14 +7,43 @@
 
 Primero debe descargar el codigo fuente que se encuentra en el archivo **decision_tree.py**. 
 
-
-Para poder correr el codigo debe verificar que tiene las librerias necesarias instaladas, en este caso se trata de numpy asi que en una consola corra el siguiente comando
-
-    pip install numpy
+Lo recomendable es crear una carpeta y guardar el archivo en la misma para después abrir la carpeta con algún editor de código de preferencia. En mi caso ocupo [VS Code](https://code.visualstudio.com/) para poder correr el archivo. 
 
 
-Una vez verificado que se tiene la libreria puede ejecutar el siguiente comando en la consola para correr el codigo de la implementacion.
+Una vez abierta la carpeta en su editor de código debe iniciar una terminal y verificar que el directorio se encuentra en la ruta donde está el archivo **decision_tree.py**
+
+Lo siguiente será crear un entorno virtual de python para instalar las librerías dentro de él y que no afecte otro lado. Para hacer esto debe correr el siguiente comando
+
+    python -m venv nombreEntornoVirtual
+
+** Puede cambiar el nombre del entorno remplazando _nombreEntornoVirtual_ por el que quiera.
+
+Una vez que se ha creado el entorno virtual se debe activar, para esto debe correr
+
+    nombreEntornoVirtual\Scripts\activate
+
+Podrá observar en la terminal que el directorio cambiará y tendrá el siguiente formato
+
+**(nombreEntornoVirtual)** PS C:\Users\TuRuta
+
+Una vez creado e inicializado el entrorno virtual podemos instalar las librerías necesarias para poder correr el código así que ingresa el siguiente comando en la terminal
+
+    pip install numpy scikit-learn
+
+El comando anterior permite instalar numpy que se ocupa para poder separar el dataset y hacer modificaciones. Scikit-learn se ocupa para evaluar la eficiencia del código. Una vez que se tienen las librerías instaladas puede ejecutar el siguiente comando en la consola para correr el codigo de la implementacion.
 
     python decision_tree.py
 
-En el codigo puede cambiar los datos de entrenamiento como los datos para hacer el testing que estan identificados con las variables _x_train_,  _y_train_ y _x_test_
+Una vez que se haya corrido el código en la terminal se podrá observar 2 corridas como en la siguiente imagen. 
+![Alt text](image.png)
+
+Las corridas siempre saldrán diferentes ya que el código está diseñado para que genere datos random en cada una de las corridas así que nunca serán las mismas.
+
+Para desactivar el entorno virtual de python debe correr el siguiente comando en la terminal
+
+    deactivate
+
+** Si quiere volver a correr el código, asegúrese de que tiene el entorno virtual activo ya que de lo contrario marcará error
+
+
+
--- decision_tree.py
+++ decision_tree.py
@@ -1,25 +1,40 @@
 # Paula Sophia Santoyo Arteaga
 # A01745312
-# 28-Ago-2023
+# 04-Sept-2023
 # Implementación de una técnica de aprendizaje máquina
 # ----------------------------------------------------
 
+
 # Importar librerias
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 
 # Representa un nodo dentro del árbol de decisión
+# Almacena la información del indice Gini, la predicción, los nodos 
+# hijos (izquierdo y derecho), el atributo y el umbral
 class DecisionNode:
     def __init__(self, gini, prediction, left=None, right=None, 
-                 attribute=None, threshold=None): 
+                 attribute=None, threshold=None):
+        # Índice gini
         self.gini = gini
+        # Predicción
         self.prediction = prediction
+        # Nodo izquierdo
         self.left = left
+        # Nodo derecho
         self.right = right
+        # Atributo a evaluar
         self.attribute = attribute
+        # Umbral de decisión
         self.threshold = threshold
 
-# Función que calcula el índice Gini para medir la impureza de las muestras
 def gini(y):
+    """
+    Función que calcula la impureza del índice gini. Se usa para 
+    calcular la probabilidad de que la muestra seleccionada sea 
+    clasificada de forma incorrecta    
+    """
     classes = np.unique(y)
     total_samples = len(y)
     gini = 1.0
@@ -30,95 +45,137 @@ def gini(y):
 
     return gini
 
-# Función que divide los datos en izquierda y derecha de acuerdo al attribute y threshold
 def split_data(x, y, attribute, threshold):
+    """
+    Función que divide el conjunto de datos en 2 conjuntos (x y y) para
+    el lado izquierdo y derecho de acuerdo al atributo y umbral
+    """
+    # El atributo es menor o igual al umbral
     left_side = x[:, attribute] <= threshold
+    # El atributo es mayor al umbral
     right_side = x[:, attribute] > threshold
+    # Almacena lo que va en el lado izquierdo y derecho
     x_left, y_left = x[left_side], y[left_side]
     x_right, y_right = x[right_side], y[right_side]
     return x_left, y_left, x_right, y_right
 
-# Función que construye el árbol de decisión 
 def build_tree(x, y, current_depth, max_depth, indent=""):
-    # Se decide si se debe detener la construcción del árbol o continua
+    """
+    Construye el árbol de decisión de forma recursiva con la profundidad
+    máxima permitida. Utiliza el índice Gini para elegir el mejor atributo de división
+    """
     if current_depth >= max_depth or len(np.unique(y)) == 1:
         predominant_class = np.bincount(y).argmax()
         return DecisionNode(gini=0, prediction=predominant_class)
 
+    # Se inicializan variables
     best_gini = float('inf')
     best_attribute = None
     best_threshold = None
 
-    # Busca encontrar el mejor attribute y threshold para dividir los datos en el árbol
     for attribute in range(x.shape[1]):
+        # Se obtienen los valores únicos de los atributos
         unique_values = np.unique(x[:, attribute])
         for threshold in unique_values:
             x_left, y_left, x_right, y_right = split_data(x, y, attribute, threshold)
             if len(y_left) > 0 and len(y_right) > 0:
+                # Calcula el índice Gini para el lado izquierdo y derecho
                 gini_left = gini(y_left)
                 gini_right = gini(y_right)
+                # Se calcula el índice Gini ponderado
                 weighted_gini = (len(y_left) / len(y)) * gini_left + (len(y_right) / 
                                 len(y)) * gini_right
+                # Compara el valor del índice Gini para encontrar el mejor y obtener el mejor atributo
+                # y umbral
                 if weighted_gini < best_gini:
                     best_gini = weighted_gini
                     best_attribute = attribute
                     best_threshold = threshold
 
-    # Si no se encuentra una división óptima se crea un nodo hoja para predecir la clase predominante
     if best_attribute is None:
+        # Se calcula la clase predominante en caso de no encontrar el mejor atributo para dividir
         predominant_class = np.bincount(y).argmax()
         print(f"{indent}Prediction: {predominant_class}")
         return DecisionNode(gini=0, prediction=predominant_class)
 
-    # Se construyen los nodos izquierdo y derecho del árbol de decisión 
+    # Realiza la división en derecha e izquierda de acuerdo con el mejor umbral y atributo encontrados
     x_left, y_left, x_right, y_right = split_data(x, y, best_attribute, best_threshold)
+    # Se crea el árbol izquierdo
     left_node = build_tree(x_left, y_left, current_depth + 1, max_depth, indent + "  ")
+    # Se crea el árbol derecho
     right_node = build_tree(x_right, y_right, current_depth + 1, max_depth, indent + "  ")
     
     return DecisionNode(gini=best_gini, prediction=None, left=left_node, right=right_node, 
                         attribute=best_attribute, threshold=best_threshold)
 
-# Función que realiza predicciones a partir de una muestra
 def tree_predict(node, sample):
+    """
+    Función que toma el nodo raíz y una muestra para predecir siguiendo el árbol de decisión    
+    """
     if node.prediction is not None:
         return node.prediction
     if sample[node.attribute] <= node.threshold:
         return tree_predict(node.left, sample)
     else:
         return tree_predict(node.right, sample)
+    
+def load_different_datasets():
+    """
+    Función que genera diferentes datos de entrenamiento y testing 
+    """
+    # Datos de entrenamiento aleatorios
+    x_train = np.random.rand(100, 10) 
+    y_train = np.random.randint(2, size=100)  
+    
+    # Datos para testing aleatorios
+    x_test = np.random.rand(20, 10) 
+    y_test = np.random.randint(2, size=20)
 
-# --------------------------------------------------------------------------
+    return x_train, y_train, x_test, y_test
 
-# SECCIÓN DE DATOS A OCUPAR
 
-# Profundidad máxima (niveles desde el nodo raíz)
-max_depth = 5
+# Manda llamar a la función load_different_datasets() para asignarle los valores 
+# random a cada una de las 4 variables.Representantan los valores de entrenamiento y testing
+x_train, y_train, x_test, y_test = load_different_datasets()
 
-# Datos de entrenamiento
-x_train = np.array([
-    [5, 2],
-    [3, 8],
-    [8, 6],
-    [1, 4],
-    [9, 1]
-])
 
-y_train = np.array([0, 1, 1, 0, 1])
+# Divide el dataset de entrenamiento en 2 partes
+x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
 
-# Datos para testing
-x_test = np.array([
-    [4, 3],
-    [7, 5],
-    [2, 4],
-    [8, 2],
-    [6, 7],
-    [1, 5]
-])
 
-# -----------------------------------------------------------------------
+def depth_Tree(depth):
+    """
+    Función que toma la profundidad máxima como entrada y construye un árbol de decisión con 
+    esa profundidad. Realiza predicciones calculando las métricas como precisión, recall y 
+    matriz de confusión
+    """
+    # Determina la profundidad máxima que tendrá el árbol de decisión
+    max_depth = depth
+    tree = build_tree(x_train, y_train, current_depth=0, max_depth=max_depth)
 
-tree = build_tree(x_train, y_train, current_depth=0, max_depth=max_depth)
+    # Realizar predicciones en los datos de prueba
+    predictions = [tree_predict(tree, sample) for sample in x_test]
 
-for sample in x_test:
-    prediction = tree_predict(tree, sample)
-    print(f"Para la muestra {sample}, la prediccion es: {prediction}")
+    # ---- Calcular métricas de evaluación ----
+    
+    # Precisión general del modelo
+    accuracy = accuracy_score(y_test, predictions) 
+    # Mide la proporción de positivos
+    precision = np.round(precision_score(y_test, predictions, zero_division=0), 2) 
+    # Mide la proporción de positivos que son predichos correctamente
+    recall = np.round(recall_score(y_test, predictions), 2) 
+    # Almacena la matriz de confusión
+    conf_matrix = confusion_matrix(y_test, predictions) 
+
+    # Muestra los resultados
+    print(f"Accuracy: {accuracy}")
+    print(f"Precision: {precision}")
+    print(f"Recall: {recall}")
+    print(f"Confusion Matrix:\n{conf_matrix}")
+
+    
+# El código se corre dos veces con profundidades diferentes y datos diferentes
+print('========= CORRIDA 1 ==========\n     Maxima profundidad: 3\n')
+depth_Tree(3)
+print('========= CORRIDA 2 ==========\n     Maxima profundidad: 7\n')
+depth_Tree(7)
